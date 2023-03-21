f41673c436b6e4886d33573a8c12a18881e5b9d5
Matias Camiletti <matias.camiletti@gmail.com>
2023-03-21 09:31:10 -0300

    Create library structure.

---
+++ projects/tots/loading/src/lib/loading.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-loading',
+  template: `
+    <p>
+      loading works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class LoadingComponent {
+
+}
---
+++ projects/tots/loading/src/lib/loading.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { LoadingComponent } from './loading.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoadingComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    LoadingComponent
+  ]
+})
+export class LoadingModule { }
